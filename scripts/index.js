ab56833c71245e609bb475f484bf9d25c8cee79d
marina138 <marinalime36@gmail.com>
2024-12-18 18:10:02 +0300

    Add aray cards

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+import { renderCards, createCard } from './cards.js';
+
+// Массив начальных карточек
+const initialCards = [
+    { name: "Архыз", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_1.jpg" },
+    { name: "Челябинская область", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_2.jpg" },
+    { name: "Иваново", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_3.jpg" },
+    { name: "Камчатка", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_3.jpg" },
+    { name: "Холмогорский район", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_3.jpg" },
+    { name: "Байкал", link: "C:\Users\marin\OneDrive\Desktop\СайтДля ЛЁхи\InternetTech\images\card_3.jpg" }
+];
